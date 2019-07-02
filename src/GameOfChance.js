c3535d53e03b658476bd69cef1ef97389865c51f
Marlo Boticano <m.boticano@gmail.com>
2019-07-02 15:18:13 -0700

    Alex's code w/ modifications

---
+++ src/GameOfChance.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import Results from './Results';
+
+
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1,
+      wins: 0,
+      loses: 0,
+      expression: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.updateWins = this.updateWins.bind(this);
+    this.whenWins = this.whenWins.bind(this);
+    this.updateLoses = this.updateLoses.bind(this);
+    this.whenLoses = this.whenLoses.bind(this);
+  }
+  handleClick() {
+    let expression = Math.random() > .5;
+    this.setState({
+      expression: expression,
+      counter: this.state.counter + 1// change code here
+    });
+  }
+  updateWins() {
+    this.setState({
+      wins: this.state.wins + 1
+    })
+  }
+
+  updateLoses() {
+    this.setState({
+      loses: this.state.loses + 1
+    })
+  }
+  
+  whenWins(){
+    this.updateWins();
+    return <Results fiftyFifty="You Win!"/>;
+  }
+
+  whenLoses() {
+    this.updateLoses();
+    return <Results fiftyFifty = "You Lose!"/>;
+  }
+  
+  render() {
+    let expression = this.state.expression; // change code here
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        { /* change code below this line */ }
+        {(expression === 1) ? this.whenWins : this.whenLoses }
+        { /* change code above this line */ }
+        <p>{'Turn: ' + this.state.counter}</p>
+        <p>{'Wins: ' + this.state.wins}</p>
+      </div>
+    );
+  }
+};
+
+export default GameOfChance;
---
+++ src/Results.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Results extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    return (
+      <h1>
+      {
+        /* change code here */
+        this.props.fiftyFifty
+      }
+      </h1>
+    )
+  };
+};
+
+export default Results;
